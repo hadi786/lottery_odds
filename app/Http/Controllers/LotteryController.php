94fc7c6bbd8b836d69b46ed21f6aa2f3503c09f8
Ali imran <hadisoft786@gmail.com>
2022-04-01 00:00:56 +0500

    calculate odds and retrun json.

--- app/Http/Controllers/LotteryController.php
+++ app/Http/Controllers/LotteryController.php
@@ -10,25 +10,35 @@ class LotteryController extends Controller
     $n = $request->balls_main_pool;
     $r = $request->balls_drawn;
 
-    $numerator_value = $this->calculate_odds($n, $r);
+    $numerator_value = $this->calculate_combinations($n, $r);
 
-    for($m =  1; $m <= $n; $m++){
-        $denominator_part_one = $this->calculate_odds($r, $m);
-        $denominator_part_two = $this->calculate_odds(($n - $r), ($r - $m));
-        $data_set[] = $numerator_value / ($denominator_part_one * $denominator_part_two);
+    $data_set = array();
+    for($m = $r; $m > 0; $m--){
+      $denominator_part_one = $this->calculate_combinations($r, $m);
+      $denominator_part_two = $this->calculate_combinations(($n - $r), ($r - $m));
+      $result_value = round($numerator_value / ($denominator_part_one * $denominator_part_two));
+      $result_value = number_format($result_value);
+
+      $item = array("number_matched" => $m, "calucated_odds" => "1 of " . $result_value);
+
+      array_push($data_set,$item);
     }
 
     return response()->json($data_set, 200);
   }
 
-  private function calculate_odds($n, $r){
+  private function calculate_combinations($n, $r){
+    // change $r to $n in denomiator first value before muliplication
     return $this->factorial($n)  / ($this->factorial($r) * $this->factorial($n-$r));
   }
 
-  private function factorial($int) {
-    if ($int == 0) {
-     return 1;
+  private function factorial($n) {
+    $fact = 1;
+    while($n > 0)
+    {
+      $fact = $fact * $n;
+      $n--;
     }
-    return $this->factorial($int-1) * $int;
+    return $fact;
    }
 }
